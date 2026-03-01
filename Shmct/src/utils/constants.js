74ba34ad83dd22fe9684e1de51f179a72f692a60
Jayesh242663 <jayeshchanne9@gmail.com>
2026-03-01 20:49:12 +0530

    New Fee Modal Changes

--- Shmct/src/utils/constants.js
+++ Shmct/src/utils/constants.js
@@ -65,7 +65,7 @@ export const INSTITUTION_INFO = {
   phone: '0251-2800090 / 9699129153 / 9029043425',
   email: 'info@sarathishmct.com',
   website: 'www.sarathishmct.com',
-  logoPath: '/logo.png',
+  logoPath: '/favicon.png',
   approvalText: 'Approved By NCVSTE New Delhi, Recognized By Govt. of India',
   terms: [
     'Fees Once Paid will not be Refunded.',
@@ -75,9 +75,9 @@ export const INSTITUTION_INFO = {
 
 // Course options for receipt display
 export const COURSE_OPTIONS_RECEIPT = [
-  { id: 1, label: 'BSC. in Hotel Management.' },
+  { id: 1, label: 'BSC in Hotel Management' },
   { id: 2, label: 'Diploma in Hotel Management' },
   { id: 3, label: 'International Diploma in Hotel Management' },
   { id: 4, label: 'Certificate in' },
-  { id: 5, label: 'Other' },
+  { id: 5, label: 'Others' },
 ];
